9d0035c4f9b69463dffcb6d6587485bb3a7bc750
YKGDWADE3 <superwadeg3@gmail.com>
2015-09-20 16:08:37 +0800

    添加了复选框，看用户是否保存密码

---
+++ AndroidTest/SharedPreferencesTest/app/src/main/java/com/example/administrator/sharedpreferencestest/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.administrator.sharedpreferencestest;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    private Button saveData;
+    private Button restoreData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        saveData = (Button) findViewById(R.id.save_data);
+        restoreData = (Button) findViewById(R.id.restore_data);
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
+                editor.putString("name","Tom");
+                editor.putInt("age", 28);
+                editor.putBoolean("married", false);
+                editor.commit();
+            }
+        });
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences pref = getSharedPreferences("data",MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married",true);
+                Log.d("MainActivity","name is "+ name);
+                Log.d("MainActivity","age is "+ age);
+                Log.d("MainActivity","married is "+ married);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
