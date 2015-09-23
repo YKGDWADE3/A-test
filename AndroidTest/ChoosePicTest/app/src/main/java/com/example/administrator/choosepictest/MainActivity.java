1a1a8fd8d15e3c246fb0dc032fac54c457f663a7
YKGDWADE3 <superwadeg3@gmail.com>
2015-09-23 21:44:14 +0800

    启动拍照程序，裁剪之后显示

---
+++ AndroidTest/ChoosePicTest/app/src/main/java/com/example/administrator/choosepictest/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.administrator.choosepictest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivity extends Activity {
+    public static final  int TAKE_PHOTO = 1;
+    public static final  int CROP_PHOTO = 2;
+    private Button takePhoto;
+    private ImageView picture;
+    private Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        takePhoto =(Button) findViewById(R.id.take_photo);
+        picture = (ImageView) findViewById(R.id.picture);
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File outputImage = new File(Environment.getExternalStorageDirectory()/*获取手机sd卡的根目录*/,"tempImage.jpg");
+                try{
+                    if (outputImage.exists()){
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+                imageUri = Uri.fromFile(outputImage);//File对象转换成Uri对象
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);//指定图片的输出地址
+                startActivityForResult(intent,TAKE_PHOTO);//启动相机程序
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        switch (requestCode){
+            case TAKE_PHOTO:
+                if(resultCode==RESULT_OK){
+                    Intent intent =new Intent("com.android.camera.action.CROP");
+                    intent.setDataAndType(imageUri,"image/*");
+                    intent.putExtra("scale", true);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                    startActivityForResult(intent,CROP_PHOTO);//启动裁剪程序
+                }
+                break;
+            case CROP_PHOTO:
+                if (requestCode == RESULT_OK){
+                    try{
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+                    }catch (FileNotFoundException e){
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
