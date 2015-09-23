2a3b1f1c5beb888a5258703557a24f7032344f0d
YKGDWADE3 <superwadeg3@gmail.com>
2015-09-23 15:33:51 +0800

    发送短信，并提示成功与否

--- AndroidTest/SMSTest/app/src/main/java/com/example/administrator/smstest/MainActivity.java
+++ AndroidTest/SMSTest/app/src/main/java/com/example/administrator/smstest/MainActivity.java
@@ -1,21 +1,34 @@
 package com.example.administrator.smstest;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.service.carrier.CarrierMessagingService;
+import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     private TextView sender;
     private TextView content;
     private IntentFilter receiveFilter;
     private MessagerReceiver messagerReceiver;
+    private EditText to;
+    private EditText msgInput;
+    private Button send;
+    private CarrierMessagingService.SendSmsResult sendSmsResult;
+    private IntentFilter sendFilter;
+    private SendStatusReceiver sendStatusReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +40,40 @@ public class MainActivity extends Activity {
         receiveFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
         receiveFilter.setPriority(100);
         messagerReceiver = new MessagerReceiver();
-        registerReceiver(messagerReceiver,receiveFilter);
+        registerReceiver(messagerReceiver, receiveFilter);
+        to = (EditText) findViewById(R.id.to);
+        msgInput = (EditText) findViewById(R.id.msg_input);
+        sendFilter = new IntentFilter();
+        sendFilter.addAction("SENT_SMS_ACTION");
+        sendStatusReceiver=new SendStatusReceiver();
+        registerReceiver(sendStatusReceiver, sendFilter);
+        send = (Button) findViewById(R.id.send);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SmsManager smsManager = SmsManager.getDefault();
+                Intent intent = new Intent("SENT_SMS_ACTION");
+                PendingIntent pi = PendingIntent.getBroadcast(MainActivity.this,0,intent,0);
+                smsManager.sendTextMessage(to.getText().toString(),null,msgInput.getText().toString(),pi,null);
+            }
+        });
+    }
+    class SendStatusReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context,Intent intent){
+            if(getResultCode()==RESULT_OK){
+                Toast.makeText(context,"Send succeeded",Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(context,"Send failed",Toast.LENGTH_LONG).show();
+            }
+        }
     }
     @Override
     protected void onDestroy(){
         super.onDestroy();
         unregisterReceiver(messagerReceiver);
+        unregisterReceiver(sendStatusReceiver);
     }
 
     class MessagerReceiver extends BroadcastReceiver{
