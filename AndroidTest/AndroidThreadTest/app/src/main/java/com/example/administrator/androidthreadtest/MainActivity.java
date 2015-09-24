ec05233843d854d53a3cad2536ae23404b6d05a3
YKGDWADE3 <superwadeg3@gmail.com>
2015-09-24 20:42:41 +0800

    体会线程，UI只能在主进程里操作

---
+++ AndroidTest/AndroidThreadTest/app/src/main/java/com/example/administrator/androidthreadtest/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.administrator.androidthreadtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Looper;
+import android.os.Message;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+    public static final int UPDATE_TEXT = 1;
+    private TextView text;
+    private Button changeText;
+    private android.os.Handler handler = new android.os.Handler(){
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UPDATE_TEXT:
+                    text.setText("Nice to meet you");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text);
+        changeText = (Button) findViewById(R.id.change_text);
+        changeText.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.change_text:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Message message = new Message();
+                        message.what = UPDATE_TEXT;
+                        handler.sendMessage(message);
+
+                    }
+                }).start();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
